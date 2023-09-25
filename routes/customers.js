1e2645dddfbd6fb44a57c99b8b1337ed811d66f2
vickeyorton <vigneshmooorthi@gmail.com>
2023-09-26 02:47:38 +0530

    CRUD service done

--- routes/customers.js
+++ routes/customers.js
@@ -1,53 +1,112 @@
 const errors = require("restify-errors");
+const rjwt = require("restify-jwt-community");
 const Customer = require("../models/Customer");
+const config = require("../config");
 
 module.exports = (server) => {
   // Get Customers
-  server.get("/customers", (req, res, next) => {
-    console.log("customer :", Customer);
+  server.get("/customers", async (req, res, next) => {
     try {
-      const Customers = Customer.find({});
-      res.send(Customers);
-      console.log("customers :", Customers);
+      const customers = await Customer.find({});
+      res.send(customers);
       next();
     } catch (err) {
       return next(new errors.InvalidContentError(err));
     }
   });
 
-  // Get  Single Customer
-  server.get("/customer/:id", (req, res, next) => {
+  // Get Single Customer
+  server.get("/customers/:id", async (req, res, next) => {
     try {
-      const Customers = Customer.findById(req.params.id);
-      res.send(Customers);
+      const customer = await Customer.findById(req.params.id);
+      res.send(customer);
       next();
     } catch (err) {
       return next(
         new errors.ResourceNotFoundError(
-          `There is no customer with Id of ${req.params.id}`
+          `There is no customer with the id of ${req.params.id}`
         )
       );
     }
   });
 
-  // Add Customers
-  server.post("/customers", (req, res, next) => {
-    if (!req.is("application/json")) {
-      return next(new errors.InvalidContentError('Expects "application/json"'));
-    }
-    const { name, email, balance } = req.body;
+  // Add Customer
+  server.post(
+    "/customers",
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is("application/json")) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
 
-    const customer = new Customer({
-      name,
-      email,
-      balance,
-    });
-    try {
-      const newCustomer = customer.save();
-      res.send(201);
-      next();
-    } catch (err) {
-      return next(new errors.InternalError(err.message));
+      const { name, email, balance } = req.body;
+
+      const customer = new Customer({
+        name,
+        email,
+        balance,
+      });
+
+      try {
+        const newCustomer = await customer.save();
+        res.send(201);
+        next();
+      } catch (err) {
+        return next(new errors.InternalError(err.message));
+      }
     }
-  });
+  );
+
+  // Update Customer
+  server.put(
+    "/customers/:id",
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is("application/json")) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
+
+      try {
+        const customer = await Customer.findOneAndUpdate(
+          { _id: req.params.id },
+          req.body
+        );
+        res.send(200);
+        next();
+      } catch (err) {
+        return next(
+          new errors.ResourceNotFoundError(
+            `There is no customer with the id of ${req.params.id}`
+          )
+        );
+      }
+    }
+  );
+
+  // Delete Customer
+  server.del(
+    "/customers/:id",
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      try {
+        const customer = await Customer.findOneAndRemove({
+          _id: req.params.id,
+        });
+        res.send(204);
+        next();
+      } catch (err) {
+        return next(
+          new errors.ResourceNotFoundError(
+            `There is no customer with the id of ${req.params.id}`
+          )
+        );
+      }
+    }
+  );
 };
